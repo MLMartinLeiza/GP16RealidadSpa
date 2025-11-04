354b3fdcfeefdf994f36bdece664c6e5b21fc534
Gomez Santiago <santygomez355@gmail.com>
2025-11-04 19:21:16 -0300

    jcalendar

--- SpaRelax1/src/Vista/Instalacion.java
+++ SpaRelax1/src/Vista/Instalacion.java
@@ -31,11 +31,11 @@ public class Instalacion extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 567, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 453, Short.MAX_VALUE)
         );
 
         pack();
--- SpaRelax1/src/Vista/Tratamiento.java
+++ SpaRelax1/src/Vista/Tratamiento.java
@@ -31,11 +31,11 @@ public class Tratamiento extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 514, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 406, Short.MAX_VALUE)
         );
 
         pack();

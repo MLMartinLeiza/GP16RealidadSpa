82ec6652c1fa069ef77836228b72c3dcc90e7214
Gomez Santiago <santygomez355@gmail.com>
2025-11-11 20:48:58 -0300

    Ultima base de datos con VistaInstalacion echa

--- spag16.sql
+++ spag16.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.3-1.fc43
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 11-11-2025 a las 16:38:43
--- Versión del servidor: 10.11.13-MariaDB
--- Versión de PHP: 8.4.14
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-11-2025 a las 00:48:06
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,7 +44,10 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`codCli`, `dni`, `nombreCompleto`, `telefono`, `edad`, `afecciones`, `estado`) VALUES
-(1, 38749542, 'Martin Leiza', '0000393426', 30, 'Nada', 1);
+(1, 44020403, 'Gomez Santiago', '0000393498', 24, 'Sin afecciones', 1),
+(4, 38749542, 'Martin Leiza', '0000393426', 30, 'Nada', 1),
+(9, 40523484, 'Eduardo Gimenes', '53982367', 35, 'Sin afecciones', 1),
+(10, 45886532, 'Milagros Alfaro', '53982367', 21, 'Sin afecciones', 1);
 
 -- --------------------------------------------------------
 
@@ -74,14 +77,6 @@ CREATE TABLE `dia_de_spa` (
   `estado` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `dia_de_spa`
---
-
-INSERT INTO `dia_de_spa` (`codPack`, `fecha_hora`, `preferencias`, `codCli`, `monto`, `estado`) VALUES
-(2, '2025-11-19 08:00:00', 'Masaje mas relajante', 1, 20000, 1),
-(3, '2025-11-21 10:00:00', 'Masaje relajante', 1, 25000, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -90,11 +85,22 @@ INSERT INTO `dia_de_spa` (`codPack`, `fecha_hora`, `preferencias`, `codCli`, `mo
 
 CREATE TABLE `instalacion` (
   `codInstal` int(10) NOT NULL,
-  `nombre` varchar(80) NOT NULL,
-  `detalle_uso` varchar(200) NOT NULL,
-  `precio_30min` decimal(10,2) NOT NULL,
+  `nombre` varchar(80) DEFAULT NULL,
+  `detalle_uso` varchar(200) DEFAULT NULL,
+  `precio_30min` decimal(10,2) DEFAULT NULL,
   `estado` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `instalacion`
+--
+
+INSERT INTO `instalacion` (`codInstal`, `nombre`, `detalle_uso`, `precio_30min`, `estado`) VALUES
+(1, 'Pileta Climatizada', 'Acceso con agua a 30°C. Capacidad para 10 personas.', 600.00, 0),
+(2, 'Sala de Meditación', 'Espacio silencioso con aromaterapia.', 123.00, 1),
+(3, 'Jacuzzi o hidromasaje', 'Bañeras con chorros de agua a presión para relajar los músculos y aliviar la tensión.', 200.00, 0),
+(4, 'Duchas de sensaciones', ': Combina diferentes temperaturas, presiones y aromas para estimular los sentidos.', 700.00, 1),
+(5, 'Circuito de hidroterapia', 'Secuencia de 3 bañeras de agua con diferentes temperaturas para estimular la circulación.', 6456.00, 1);
 
 -- --------------------------------------------------------
 
@@ -119,7 +125,7 @@ CREATE TABLE `masajista` (
 CREATE TABLE `sesion` (
   `codSesion` int(10) NOT NULL,
   `fecha_hora_inicio` datetime NOT NULL,
-  `fecha_hora_fin` datetime DEFAULT NULL,
+  `fecha_hora_fin` datetime NOT NULL,
   `codTratamiento` int(10) DEFAULT NULL,
   `nroConsultorio` int(10) DEFAULT NULL,
   `matricula` int(10) DEFAULT NULL,
@@ -142,14 +148,6 @@ CREATE TABLE `tratamiento` (
   `costo` double NOT NULL,
   `activo` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `tratamiento`
---
-
-INSERT INTO `tratamiento` (`codTratam`, `nombre`, `detalle`, `duracion`, `costo`, `activo`) VALUES
-(1, 'Masaje Sueco', 'Terapia de relajación de cuerpo completo con presión ligera a media', 30, 5000, 1),
-(2, 'Masaje de tejido profundo', 'Para aliviar tensión muscular crónica y tratar contracturas, con presión más intensa', 30, 5000, 1);
 
 --
 -- Índices para tablas volcadas
@@ -212,7 +210,7 @@ ALTER TABLE `tratamiento`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `codCli` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `codCli` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `consultorio`
@@ -224,7 +222,13 @@ ALTER TABLE `consultorio`
 -- AUTO_INCREMENT de la tabla `dia_de_spa`
 --
 ALTER TABLE `dia_de_spa`
-  MODIFY `codPack` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `codPack` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `instalacion`
+--
+ALTER TABLE `instalacion`
+  MODIFY `codInstal` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `sesion`
@@ -236,7 +240,7 @@ ALTER TABLE `sesion`
 -- AUTO_INCREMENT de la tabla `tratamiento`
 --
 ALTER TABLE `tratamiento`
-  MODIFY `codTratam` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `codTratam` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
